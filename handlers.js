2ac08ec4d9a09f6b21c0ed6abbb92d85a95dc49e
zagorny95 <krzysztof.podworny@wp.pl>
2022-01-23 03:56:50 +0100

    V 1.0.1

---
+++ handlers.js
@@ -0,0 +1,58 @@
+const dns = require("dns");
+const res = require("express/lib/response");
+const fs = require("fs");
+
+require("express");
+
+let database = [];
+
+let regex = /^(http|https):\/\/[a-zA-z0-9]*\.[a-z]{1,}$/g;
+
+let urlgenerator = (url, req, res) => {
+  let dataFile = [];
+  let jsonFile = {};
+  fs.readFile("./database.txt", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err + "błąd");
+      return null;
+    } else {
+      dataFile = data.split(",");
+      jsonFile = { original_url: url, short_url: dataFile.length - 1 };
+      res.send(jsonFile);
+      database = dataFile;
+      return;
+    }
+  });
+};
+
+let generateUrl = (req, res) => {
+  let url = req.body.short_url;
+  if (regex.test(url)) {
+    fs.writeFile(`./database.txt`, `,${url}`, { flag: "a+" }, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Saved!");
+        urlgenerator(url, req, res);
+      }
+    });
+  } else {
+    res.send({ error: "invalid url" });
+  }
+};
+
+let redirect = (req, res) => {
+  let id = req.params.short_url;
+  console.log(database[id]);
+  res.redirect(database[id]);
+};
+
+let getHomepage = (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+};
+
+module.exports = {
+  getHomepage,
+  generateUrl,
+  redirect,
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+let express = require("express");
+const handlers = require("./handlers");
+let app = express();
+var bodyParser = require("body-parser");
+let cors = require('cors')
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/public", express.static("/public"));
+
+app.get("/", handlers.getHomepage);
+
+app.get("/api/shorturl/:short_url", handlers.redirect);
+
+app.post("/api/shorturl", handlers.generateUrl);
+
+app.listen(3000, () => console.log("server listening on port 3000..."));
